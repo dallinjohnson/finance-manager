dd3d54df34ea7af4e7fcc055cb698a1b6dd7520c
Dallin Johnson <13dallin13@gmail.com>
2025-03-14 14:09:23 -0600

    Reorganize folder structure for main route components

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, inject } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { AuthService } from '../../services/auth-service';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-login',
